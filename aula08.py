822cf1d1f6b5533d9b085e409b8e4122188f1a65
juliana gomes da silva <julianacore2@gmail.com>
2023-04-13 15:42:17 -0400

    Main

--- aula08.py
+++ aula08.py
@@ -1,8 +1,36 @@
+for numero in range(0,50,3):
+    print("Número:" + str(numero))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Contagem regressiva
 
-numero = int(input('Digite um número:'))
+#numero = int(input('Digite um número:'))
 
-while(numero >= 0):
-    print('numero: ' + str(numero))
-    numero = numero - 1
-print('Chegou a zero!!!')
+#while(numero >= 0):
+    #print('numero: ' + str(numero))
+    #numero = numero - 1
+#print('Chegou a zero!!!')
+
+#nMastigadas = 0
+#tipoComida = 'sopa'
+#while(nMastigadas <20):
+    #if (tipoComida == 'sopa'):
+        #print("É sopa, pode engolir!")
+        #break
+#print("Mastigada de número: " + str(nMastigadas))
+#nMastigadas = nMastigadas + 1
+
+
+
