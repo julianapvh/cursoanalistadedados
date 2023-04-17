ff5dae0c961545b4b30a1e1621b728d076dbcc6b
juliana gomes da silva <julianacore2@gmail.com>
2023-04-17 15:54:18 -0400

    Main salario

---
+++ ehmultiplo.py
@@ -0,0 +1,10 @@
+numero = int(input('Digite um número: '))
+
+if((numero % 5 == 0)):
+    print(f"O número {numero}  é multiplo de 5 ")
+        
+else:
+    print(f"O número {numero:}  não é multiplo de 5 ")
+
+
+
