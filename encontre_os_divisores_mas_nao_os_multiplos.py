76a95e6b4a8693bd2d37cc47d64737ece244e4a7
Julianapvh <70443877+julianapvh@users.noreply.github.com>
2023-04-18 20:35:48 -0400

    novo_exercicio
    update

---
+++ encontre_os_divisores_mas_nao_os_multiplos.py
@@ -0,0 +1,16 @@
+"""Temas: Entrada de dados, Saída de dados, Funções, Listas, Operadores matemáticos, Estruturas condicionais, Estruturas de repetição, Python
+
+Classificação: Intermediário
+
+O que é para fazer:
+
+Escreva um programa que encontrará todos esses números que são divisíveis por 7, 
+mas não são um múltiplo de 5, entre 2000 e 3200 (ambos incluídos).
+
+Como Fazer:
+
+Crie uma lista para armazenar os números;
+Crie um laço para percorrer o processo no intervalo solicitado;
+Crie uma condição para verificar o que foi solicitado;
+Adicione os resultados na lista criada anteriormente;
+Imprima os valores para o usuário."""
