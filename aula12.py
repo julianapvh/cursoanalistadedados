3f4d6bcce53209be200ed02860c5bdd08b9089af
juliana gomes da silva <julianacore2@gmail.com>
2023-04-20 17:04:05 -0400

    aula 12
    update aula 12

---
+++ aula12.py
@@ -0,0 +1,45 @@
+#função
+"""def criaTextoPreco(item, preco):
+    return print('O item ' + item + 'custa R$ ' +  str(preco))
+
+criaTextoPreco('Tomate ', '6,99')
+criaTextoPreco('Banana ', '5,24')
+criaTextoPreco('Cebola ', '3,97')
+criaTextoPreco('Limão ',  '0,99')"""
+
+#==============================================================================================================================================
+
+"""def liquidificador(item1, item2, item3):
+    if(item1 =='banana' and item2== 'leite' and item3== 'achocolatado'):
+        return print('Vitamina de banana')
+    else: 
+        return print('Liquidifiquei tudo!')
+    
+    
+liquidificador('Banana', 'Leite', 'Achocolatado')"""
+
+#==============================================================================================================================================
+"""idadeUsuario = int(input('digite a sua idade '))
+
+def dirigirOuBeber(idade):
+    if(idade >=18 ):
+        
+     return print('Já pode dirigir ou beber')
+    else:
+        
+     return print('Não pode nem dirigir nem beber')
+ 
+dirigirOuBeber(idadeUsuario)"""
+
+quantidade_km = int(input('Digite a quantidade de kilometros'))
+tamanho_tanque = int(input('Digite o tamanho do seu tanque'))
+
+def previsaoRodagem(quantidade, tanque):
+    
+
+
+
+
+
+
+ 
