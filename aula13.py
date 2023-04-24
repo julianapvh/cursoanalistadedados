5f92dd4a4675a928ffc7e8bd767d13ae7be4661a
juliana gomes da silva <julianacore2@gmail.com>
2023-04-24 15:58:53 -0400

    update aula 13

--- aula13.py
+++ aula13.py
@@ -1,9 +1,28 @@
 #declaração da função
 
-def produzirCamiseta(tamanho, estampa='Turma VamoAi 2023'):
-    print(f'Camiseta com a estampa {estampa} produzida no tamanho {tamanho}.')
+#def produzirCamiseta(tamanho, estampa='Turma VamoAi 2023'):
+    #print(f'Camiseta com a estampa {estampa} produzida no tamanho {tamanho}.')
     
     #chamada da função
 #produzirCamiseta(tamanho = 'M', estampa = 'turma VamoAi 2023')
 
-produzirCamiseta(tamanho = 'M')
+#produzirCamiseta(tamanho = 'M')
+
+
+#==========================================================ATIVIDADE========================================================================================================================
+
+def cidades(cidades, pais='brasil'):
+    if(cidades =='Porto Velho'):
+        print(cidades, 'é uma cidade do Brasil')
+        
+    if(cidades =='Brasilia'):
+        print(cidades, 'é uma cidade do Brasil')
+        
+    else:
+   
+        print('Está cidade não fica localizada no Brasil')
+        
+        
+cidades('Porto Velho')
+cidades('Brasilia')
+cidades('Toquio')
