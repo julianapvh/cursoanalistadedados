999f91e6ba4d79c394800d34d88831d6e8e9eca3
juliana gomes da silva <julianacore2@gmail.com>
2023-04-25 16:58:43 -0400

    atualizando aula 14

---
+++ exerciciosNasAulas/introducao_ao_modular/biblioteca.py
@@ -0,0 +1,9 @@
+def soma(valor1, valor2):
+    return valor1 + valor2
+
+
+def multiplicacao(valor1, valor2):
+    return valor1 * valor2
+
+def bb1(valor1, valor2):
+    return valor1 / valor2
---
+++ exerciciosNasAulas/introducao_ao_modular/main.py
@@ -0,0 +1,15 @@
+'''from biblioteca import *
+
+print(soma(5,7))
+print(multiplicacao(5,7))'''
+
+
+'''from biblioteca import soma
+
+print(soma(5,7))
+#print(multiplicacao(5,7))'''
+
+'''from biblioteca import multiplicacao
+
+#print(soma(5,7))
+print(multiplicacao(5,7))'''
