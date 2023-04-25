878785ee635bfe05ad9b40437b17c20af3c86dac
juliana gomes da silva <julianacore2@gmail.com>
2023-04-25 16:02:42 -0400

    Organização dos exercícios em pastas

---
+++ exerciciosNasAulas/aula14.py
@@ -0,0 +1,59 @@
+'''def soma (valor1, valor2):
+    return valor1 + valor2
+
+def calculaDobroDaSoma(var1, var2):
+    resultado = soma(var1, var2)
+    dobro = resultado * 2
+    return dobro
+    
+print(calculaDobroDaSoma(var1 = 10, var2 = 20))'''
+
+#=======================Exercicio=================================================
+
+'''criar uma nova função que será utilizada dentro do retorno da função calculaDobroDaSoma
+e essa função deve imprimir o resultado da seguinte forma:
+'A soma do valor1 mais o valor2 é igual ao resultado e seu dobro é igual ao dobro'''
+
+
+'''def soma (valor1, valor2):
+    return valor1 + valor2
+
+def calculaDobroDaSoma(var1, var2):
+    resultado = soma(var1, var2)
+    dobro = resultado * 2
+    return dobro
+
+def mostrarResultado(valor1, valor2, resultado, dobro):
+return
+    
+ print(f"A soma do {valor1} mais o {valor2} é igual a {resultado} e o seu dobro é igual a {dobro}")'''
+ 
+ 
+ #============================exemplo do professor ================================================================
+''' def soma(valor1, valor2):
+    return valor1+ valor2
+
+def imprimir(valor1, valor2, resultado, dobro):
+    print(f"A soma do {valor1} mais o {valor2} é igual a {resultado} e o seu dobro é igual a {dobro}")
+
+def calculaDobroDaSoma(valor1, valor2):
+    resultado = soma(valor1,valor2)
+    dobro = resultado * 2
+    return imprimir(valor1, valor2, resultado, dobro)
+
+calculaDobroDaSoma(2,3)'''
+    
+#============================exemplo do professor ==================================================================
+    
+'''def fatorial(numero):
+    if numero == 1:
+        return 1
+    return numero * fatorial(numero - 1)
+
+print(fatorial(3))'''
+
+
+
+    
+    
+    
--- teste.py
+++
@@ -1,6 +0,0 @@
-salario_minimo = float(input("Digite o valor do SalÃ¡rio MÃ­nimo: "))
-salario_servidor = float(input("Digite o salÃ¡rio do servidor: "))
-
-qtd_salarios = salario_servidor/salario_minimo
-
-print("A quantidade de salarios que o servidor ganha e: %.2f" % qtd_salarios)
