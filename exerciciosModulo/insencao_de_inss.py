0ac177b7fd0d6222a38f168064feb78e15849007
juliana gomes da silva <julianacore2@gmail.com>
2023-04-26 13:44:07 -0400

    projeto em grupo

---
+++ exerciciosModulo/insencao_de_inss.py
@@ -0,0 +1,16 @@
+'''Temas: Entrada de dados, Operadores matemáticos, Estruturas condicionais, Python, Saída de dados
+
+Classificação: Intermediário
+
+O que é para fazer:
+
+Escreva um programa em Python que exiba o desconto do INSS segundo seu salário. Por exemplo, se o usuário possuir salário inferior ou igual a R$: 600,00 ele será isento, em caso de maior que R$: 600,00 e menor ou igual a R$:1.200,00, o desconto é de 20%; em caso de Maior que R$: 1.200,00 e menor ou igual a R$: 2.000,00, o desconto é de 25%; e em caso de maior que R$: 2.000,00, o desconto é de 30%.
+
+Como Fazer:
+
+Crie uma variável para receber o salário do usuário;
+Crie uma condição para fazer as verificações necessárias;
+Dentro das verificações, crie a variável que vai armazenar o cálculo do desconto;
+Imprima o valor do desconto para o usuário.'''
+
+
