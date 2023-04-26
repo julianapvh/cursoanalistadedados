1ccea2fef1f27e7b9e4185f023abc68bcbec95e0
juliana gomes da silva <julianacore2@gmail.com>
2023-04-26 15:48:31 -0400

    update aula 15

--- exerciciosNasAulas/introducao_ao_modular/biblioteca_15.py
+++ exerciciosNasAulas/introducao_ao_modular/biblioteca_15.py
@@ -2,7 +2,7 @@ def soma(valor1, valor2):
     return valor1 + valor2
 
 def subtração(valor1, valor2):
-    return valor1 + valor2
+    return valor1 - valor2
 
 
 def multiplicacao(valor1, valor2):
---
+++ exerciciosNasAulas/introducao_ao_modular/main_15.py
@@ -0,0 +1,32 @@
+#++++++++++++++++++++++++++++++ exemplo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''from biblioteca_15 import *
+import biblioteca as bbl         #alias
+from biblioteca import soma
+from biblioteca import multiplicaçao
+
+
+num1 = int(input('valor 1))
+num2 = int(input('valor 2))
+
+usado para especificar que a função é de uma biblioteca especifica 
+print(biblioteca.soma(5,7))
+print(biblioteca.multiplicaçao(5,4))
+
+
+print(soma(5,7))
+print(subtração(5,7))
+print(multiplicacao(5,7))
+print(divisao(5,7))'''
+
+#++++++++++++++++++++++++++++++ exercicio
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from biblioteca_15 import soma as sm
+
+
+print(sm(int(input("Digite o primeiro número: ")), int(input('Digite o segundo número: '))))
+
+
+
+
+
+
