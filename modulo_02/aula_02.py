9fd42cfe10c8f2f210266cf666c4afa726b295fc
https://github.com/julianapvh <julianacore2@gmail.com>
2023-05-09 16:08:25 -0400

    aula 02, modulo 2

---
+++ modulo_02/aula_02.py
@@ -0,0 +1,60 @@
+'''lista01 = [10, 20, 30]
+print(lista01[0])
+
+lista02 = ['sapo', 'jacare', 'cobra']
+print(lista02[1])
+
+lista03 = [10, 20, ['sapo', 'cobra']]
+print(lista03[2])
+
+lista01[0] = 10*100
+print(lista01)
+
+concatenar = lista02 + lista03
+print(len(concatenar))'''
+
+
+
+'''lista02 = ['sapo', 'jacare', 'cobra']
+
+if 'coala' in lista02:
+    print('Cobra é um dos animais contidos na lista 02')
+else:
+    print('Não está na lista 02')'''
+
+
+
+
+#      vc pode ler assim para(for) cada item(i,) in(em) Variavel
+
+       #    Atividade 01
+'''lista_nomes = ['juliana', 'gomes', 'da silva']
+
+for i in lista_nomes:
+
+ print('Meu nome e sobrenome', [i])'''
+
+
+ #    Atividade 02
+
+
+'''lista_nomes = ['José', 'Pedro', 'João']
+
+lista_nomes2 = ['Maria', 'Serafina']
+
+concatene = lista_nomes + lista_nomes2
+
+
+if 'zé' in lista_nomes:
+    print('O nome está na lista' )
+else:
+    print('O nome não está na lista ' )'''
+
+
+
+
+
+
+
+
+
