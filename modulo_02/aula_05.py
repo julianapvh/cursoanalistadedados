c4a6eb3ac43390c626f452b691020d61c3a1b391
julianapvh <julianacore2@gmail.com>
2023-05-15 15:28:12 -0400

    aula_05

---
+++ modulo_02/aula_05.py
@@ -0,0 +1,47 @@
+
+# fila
+
+
+'''def insere_na_fila(fila, elemento):
+    return fila.append(elemento)
+    
+def retira_na_fila(fila):
+    return fila.pop(0)
+    
+fila = []
+    
+insere_na_fila(fila, "thiago")
+insere_na_fila(fila, "luciana")
+insere_na_fila(fila, "joão")
+print(fila)
+
+
+retira_na_fila(fila)
+   
+print(fila)'''
+
+
+# pilha
+
+
+def empilha(pilha, elemento):
+    return pilha.append(elemento)
+    
+def desempilhar(pilha):
+    return pilha.pop(-1)
+    
+pilha = []
+    
+empilha(pilha, "thiago")
+empilha(pilha, "luciana")
+empilha(pilha, "joão")
+print(pilha)
+
+
+desempilhar(pilha)
+   
+print(pilha)
+
+
+
+
